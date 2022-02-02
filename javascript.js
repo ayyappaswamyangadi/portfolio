3f48693731d643ef7371aac3fc513ff00b61472c
Ayyappa Swamy Angadi <48783998+ayyappaswamyangadi@users.noreply.github.com>
2022-02-02 21:33:56 +0530

    source code
    added the code part

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// const btncircle = document.querySelectorAll('.nav-link');
+
+// function col(){
+//     btncircle[i].classList.add('coloring');
+// };
+
+// btncircle.addEventListener('click', col);
