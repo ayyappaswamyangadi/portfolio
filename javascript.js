8c31b334553e9bc1a70ed4d80d277be32453dc22
Ayyappa Swamy Angadi <ayyappaswamy50@gmail.com>
2022-02-03 14:17:34 +0530

    nav-bar
    added style to navbar

--- javascript.js
+++ javascript.js
@@ -35,3 +35,27 @@ btnHome.addEventListener('click', navBtn1);
 btnaboutMe.addEventListener('click', navBtn2);
 btnmySkills.addEventListener('click', navBtn3);
 btncontactMe.addEventListener('click', navBtn4);
+
+function checkRefresh()
+{
+    window.location.hash = ('#home');
+    window.scroll(0,0);
+}
+
+var goToTopbutton = document.getElementById("top");
+
+window.onscroll = function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    goToTopbutton.style.display = "block";
+  } else {
+    goToTopbutton.style.display = "none";
+  }
+}
+
+function goToTop(){
+    window.scroll(0,0);
+    btnHome.classList.add('coloring');
+    btnaboutMe.classList.remove('coloring');
+    btnmySkills.classList.remove('coloring');
+    btncontactMe.classList.remove('coloring');
+}
