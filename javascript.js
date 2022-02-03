0f78f4e52bc0f36f0a28ac0e0f41634871a38535
Ayyappa Swamy Angadi <ayyappaswamy50@gmail.com>
2022-02-03 11:10:48 +0530

    styled button
    style to button when clicked on it

--- javascript.js
+++ javascript.js
@@ -1,7 +1,37 @@
-// const btncircle = document.querySelectorAll('.nav-link');
+const btnHome = document.getElementById('nav-home');
+const btnaboutMe = document.getElementById('nav-aboutMe');
+const btnmySkills = document.getElementById('nav-mySkills');
+const btncontactMe = document.getElementById('nav-contactMe');
 
-// function col(){
-//     btncircle[i].classList.add('coloring');
-// };
+function navBtn1(){
+    btnHome.classList.add('coloring');
+    btnaboutMe.classList.remove('coloring');
+    btnmySkills.classList.remove('coloring');
+    btncontactMe.classList.remove('coloring');
+};
 
-// btncircle.addEventListener('click', col);
+function navBtn2(){
+    btnHome.classList.remove('coloring');
+    btnaboutMe.classList.add('coloring');
+    btnmySkills.classList.remove('coloring');
+    btncontactMe.classList.remove('coloring');
+};
+
+function navBtn3(){
+    btnHome.classList.remove('coloring');
+    btnaboutMe.classList.remove('coloring');
+    btnmySkills.classList.add('coloring');
+    btncontactMe.classList.remove('coloring');
+};
+
+function navBtn4(){
+    btnHome.classList.remove('coloring');
+    btnaboutMe.classList.remove('coloring');
+    btnmySkills.classList.remove('coloring');
+    btncontactMe.classList.add('coloring');
+};
+
+btnHome.addEventListener('click', navBtn1);
+btnaboutMe.addEventListener('click', navBtn2);
+btnmySkills.addEventListener('click', navBtn3);
+btncontactMe.addEventListener('click', navBtn4);
